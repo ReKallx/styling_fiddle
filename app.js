d4e6e553d44ee2124ec0ac7585169cfa8d7b2ea0
Steven Rogers <spxrogers@gmail.com>
2015-10-20 13:06:47 -0500

    Added: user alert "goto".

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ function removeInfo() {
   alert('remove');
 }
 
+function alertGoto() {
+  alert('alert goto');
+}
+
+function alertRemove() {
+  alert('alert remove');
+}
+
 $(document).ready(function() {
 
   $('#addInfo').click(function() {addInfo();});
 
   $('#removeInfo').click(function() {removeInfo();});
 
+  $('.alert-goto').click(function() {alertGoto();});
+  
+  $('.alert-remove').click(function() {alertRemove();});
 
 
 });
